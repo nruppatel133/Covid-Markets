8ab3bb84c266ad27bfae0b4e5c1d4e85634cfb4a
Nrup R Patel <nruppatel133@gmail.com>
2020-04-01 22:38:21 -0600

    Update novelcovid.js

--- micro-scrapers/novelcovid.js
+++ micro-scrapers/novelcovid.js
@@ -2,6 +2,8 @@ const axios = require("axios");
 let covid = require("novelcovid");
 const utilities = require("../utilities");
 
+
+
 const URL = "https://corona.lmao.ninja/states";
 const keyMapping = {
   country: "country",
