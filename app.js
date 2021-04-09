e5f86a3f08d77d7ccfceba5a80ce5577a69f038b
nruppatel133 <nruppatel133@gmail.com>
2021-04-09 17:12:39 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+// app nav functionality
+(() => {
+    const overviewBtn = document.querySelector('.panel.main-app .tab-select li.overview');
+    const healthBtn = document.querySelector('.panel.main-app .tab-select li.health');
+
+    const mainAppContainer = document.querySelector('.panel.main-app');
+
+    const clearCurrentTab = () => {
+        mainAppContainer.classList.remove('overview-active');
+        mainAppContainer.classList.remove('health-active');
+    };
+
+    overviewBtn.addEventListener('click', () => {
+        clearCurrentTab();
+        mainAppContainer.classList.add('overview-active');
+        removeHashFromURL();
+    });
+
+    healthBtn.addEventListener('click', () => {
+        clearCurrentTab();
+        mainAppContainer.classList.add('health-active');
+    });
+})();
+
+// live video updates from the WHO
+(() => {
+    const overviewPanelVideoContainerElm = document.querySelector('.panel.main-app .tabs > section.overview > .video-container');
+    overviewPanelVideoContainerElm.innerHTML += `
+    <iframe width="100%" height="${convertRemToPixels(
+        27.5
+    )}" src="${liveVideoURL}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `;
+})();
